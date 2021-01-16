47c14dea4cd618d84980b3003272b9f94c9b1f63
Kübra KAYMAZ <kubra.kaymaz@std.izmirekonomi.edu.tr>
2021-01-16 23:52:59 +0300

    Proje dosyası ekle.

---
+++ ClassIntro/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ClassIntro
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            string adi = "Engin";
+            int yas = 36;
+
+            Kurs kurs1 = new Kurs();
+            kurs1.KursAdi = "C#";
+            kurs1.Egitmen = "Engin Demiroğ";
+            kurs1.İzlenmeOrani = 68;
+
+            Kurs kurs2 = new Kurs();
+            kurs2.KursAdi = "Java";
+            kurs2.Egitmen = "Kerem Demiroğ";
+            kurs2.İzlenmeOrani = 64;
+
+            Kurs kurs3 = new Kurs();
+            kurs3.KursAdi = "Python";
+            kurs3.Egitmen = "Berkay";
+            kurs3.İzlenmeOrani = 80;
+
+            Kurs[] kurslar = new Kurs[] { kurs1, kurs2, kurs3 };
+
+            Console.WriteLine(kurs1.KursAdi + " : " + kurs1.Egitmen);
+            Console.WriteLine(kurs2.KursAdi + " : " + kurs2.Egitmen);
+            Console.WriteLine(kurs3.KursAdi + " : " + kurs3.Egitmen);
+        }
+    }
+    class Kurs
+    {
+        public string KursAdi { get; set; }
+        public string Egitmen { get; set; }
+        public int İzlenmeOrani { get; set; }
+
+    }
+ 
+}
